a95af4b3c32137c02eac41d37422bd2dfdfab45a
Tsungyu Yu <ytyubox@gmail.com>
2019-09-18 22:49:13 +0800

    Subject + easy generic

---
+++ Sources/CombineHelper/GenericCurrentValueSubject.swift
@@ -0,0 +1,10 @@
+//
+//  File.swift
+//  
+//
+//  Created by 游宗諭 on 2019/9/18.
+//
+
+import Combine
+@available(iOS 13.0, *)
+typealias GenericCurrentValueSubject<Output> = CurrentValueSubject<Output, Error>
---
+++ Sources/CombineHelper/GenericPassthroughSubject.swift
@@ -0,0 +1,3 @@
+import Combine
+@available(iOS 13.0, *)
+typealias GenericPassthroughSubject<OutPut> = PassthroughSubject<OutPut,Error>
